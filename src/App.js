30f58ae3b73e4e562bec11aae969a59e24d8491e
Eric Wilson <eric.a.wilson@cerner.com>
2020-06-12 14:51:24 -0500

    Add SVG Canvas and example line showing plotting coords

--- src/App.js
+++ src/App.js
@@ -13,7 +13,20 @@ const App = () => {
   console.log(hourlyWeatherData);
   return (
     <div className="App">
-      
+      {/* Here's our SVG canvas! */}
+      <svg
+        height="100%"
+        width="100%"
+        className="svgCanvas"
+        viewBox="0 0 100 100"
+      >
+        <line 
+          x1="0" y1="0"
+          x2="100" y2="100"
+          stroke="red"
+          strokeWidth="1"
+        />
+      </svg>
     </div>
   );
 }
